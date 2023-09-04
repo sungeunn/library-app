569b90dbb3ea52360594837c07520693eaa2cf7d
sungeunn <saeun0512@gmail.com>
2023-09-04 23:42:36 +0900

    feat: POST API 개발하고 테스트하기

--- src/main/java/com/group/libraryapp/controller/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/CalculatorController.java
@@ -1,7 +1,10 @@
 package com.group.libraryapp.controller;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +14,10 @@ public class CalculatorController {
     public int addTwoNumbers(CalculatorAddRequest request) {
         return request.getNumber1() + request.getNumber2();
     }
+
+    @PostMapping("/multiply")
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNumber1() * request.getNumber2();
+    }
+
 }
